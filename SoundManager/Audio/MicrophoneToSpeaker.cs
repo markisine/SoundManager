fd24e4d2feebf08552bbd2234d0fc514954565e1
Mark <mark.stroebele@t-online.de>
2022-12-20 22:08:31 +0100

    Initial Commit Sound working :cool:

---
+++ SoundManager/Audio/MicrophoneToSpeaker.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace SoundManager.Audio;
+
+using System;
+using System.IO;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+
+    class MicrophoneToSpeaker
+    {
+        private WaveIn waveIn;
+        
+        public void Run()
+        {
+            // Den Standard-Eingabe-Endpunkt abrufen
+            MMDeviceEnumerator deviceEnumerator = new MMDeviceEnumerator();
+            MMDevice defaultInputDevice = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Console);
+
+            // Einen WaveIn-Streamer mit dem Standard-Eingabe-Endpunkt erstellen
+            waveIn = new WaveIn();
+            waveIn.DeviceNumber = 0;
+            waveIn.WaveFormat = new WaveFormat(44100, 1);
+            waveIn.DataAvailable += WaveIn_DataAvailable;
+            waveIn.StartRecording();
+            
+
+            // Aufnahme beenden und WaveIn-Streamer schließen
+            // waveIn.StopRecording();
+            // waveIn.Dispose();
+        }
+
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            // Den Standard-Ausgabe-Endpunkt abrufen
+            MMDeviceEnumerator deviceEnumerator = new MMDeviceEnumerator();
+            MMDevice defaultOutputDevice = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
+
+            // Einen WaveOut-Player mit dem Standard-Ausgabe-Endpunkt erstellen
+            WaveOut waveOut = new WaveOut();
+            waveOut.DesiredLatency = 1;
+            waveOut.DeviceNumber = 0;
+            waveOut.Init(new RawSourceWaveStream(new MemoryStream(e.Buffer), waveIn.WaveFormat));
+
+            // Wiedergabe starten
+            waveOut.Play();
+        }
+    }
+
---
+++ SoundManager/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using NAudio.Wave;
+using SoundManager.Audio;
+
+namespace SoundManager;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    
+    public MainWindow()
+    {
+        InitializeComponent();
+        
+        MicrophoneToSpeaker microphoneToSpeaker = new MicrophoneToSpeaker();
+        microphoneToSpeaker.Run();
+    }
+}
